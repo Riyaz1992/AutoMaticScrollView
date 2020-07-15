05a30ea1b2530af8cfd663a59ede0918a3c4c739
gamerockz <riyaza42874@gmail.com>
2020-07-15 23:07:35 +0530

    Filw Updated
    File Updated

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+
+public class Data : MonoBehaviour
+{
+    #region Member variables
+
+    // Public Methods
+    public GameObject countryPrefab;
+    public GameObject parent;
+
+    // Private Methods
+    [SerializeField]
+    private DataContainer dataContainer;
+    
+    // Events 
+    public delegate void DataFetch();
+    public static DataFetch OnDataFetchComplete;
+
+    #endregion
+   
+    private void Start()
+    {
+        FetchData();
+    }
+
+    #region FetchData
+
+    private void FetchData()
+    {
+        dataContainer = JsonUtility.FromJson<DataContainer>(LoadJson());
+        for (int i = 0; i < dataContainer.Data.Count; i++)
+        {
+            int _i = i;
+            GameObject newButton = Instantiate(countryPrefab) as GameObject;
+            newButton.transform.SetParent(parent.transform, false);
+            StartCoroutine(DownloadImage(dataContainer.Data[i].imageurl, newButton.GetComponent<RawImage>()));
+            newButton.GetComponent<Button>().onClick.AddListener(delegate { StartCoroutine(GoToWeb(_i)); });
+        }
+        OnDataFetchComplete?.Invoke();
+    }
+
+    #endregion
+
+    #region Helper
+
+    private string LoadJson()
+    {
+        TextAsset file = Resources.Load("Data") as TextAsset;
+        return file.text;
+    }
+
+    private IEnumerator DownloadImage(string MediaUrl, RawImage YourRawImage)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl);
+        yield return request.SendWebRequest();
+        if (request.isNetworkError || request.isHttpError)
+            Debug.Log(request.error);
+        else
+            YourRawImage.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+    }
+
+    private IEnumerator GoToWeb(int imageNo)
+    {
+        yield return new WaitForSeconds(1);
+        string url = dataContainer.Data[imageNo].weburl;
+        Application.OpenURL(url);
+    }
+
+    #endregion
+}
+
+
+
+[System.Serializable]
+public class DataContainer
+{
+    public List<DataObjects> Data;
+}
+
+
+[System.Serializable]
+public class DataObjects
+{
+    public string imageurl;
+    public string weburl;
+}
---
+++ Assets/Scripts/ScrollToSelected.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+[RequireComponent(typeof(ScrollRect))]
+public class ScrollToSelected : MonoBehaviour
+{
+
+    public float scrollSpeed = 10f;
+
+    private ScrollRect m_ScrollRect;
+    private RectTransform m_RectTransform;
+    private RectTransform m_ContentRectTransform;
+    private RectTransform m_SelectedRectTransform;
+    private GameObject selected;
+
+    private void OnEnable()
+    {
+        Data.OnDataFetchComplete += OnDataFetchComplete;
+    }
+    private void OnDisable()
+    {
+        Data.OnDataFetchComplete -= OnDataFetchComplete;
+    }
+    private void OnDataFetchComplete()
+    {
+        Invoke("GetLastChild", 1);
+    }
+
+    private void Awake()
+    {
+        Debug.Log("ScrollToSelected Awaked");
+        Init();
+       // Invoke("GetLastChild", 3);
+    }
+
+    private void Init()
+    {
+        m_ScrollRect = GetComponent<ScrollRect>();
+        m_RectTransform = GetComponent<RectTransform>();
+        m_ContentRectTransform = m_ScrollRect.content;
+    }
+
+    private void GetLastChild()
+    {
+        selected = m_ContentRectTransform.GetChild(m_ContentRectTransform.transform.childCount - 1).gameObject;
+        EventSystem.current.SetSelectedGameObject ( selected);
+    }
+
+    private void Update()
+    {
+        UpdateScrollToSelected();
+    }
+
+    private void UpdateScrollToSelected()
+    {
+
+        // grab the current selected from the eventsystem
+        // selected = EventSystem.current.currentSelectedGameObject;
+        if (Input.touchCount > 0 || Input.GetMouseButton(0))
+        {
+            Debug.Log("TouchDetected");
+            selected = null;
+        }
+
+        if (selected == null)
+        {
+            return;
+        }
+        if (selected.transform.parent != m_ContentRectTransform.transform)
+        {
+            return;
+        }
+
+        m_SelectedRectTransform = selected.GetComponent<RectTransform>();
+
+        // math stuff
+        Vector3 selectedDifference = m_RectTransform.localPosition - m_SelectedRectTransform.localPosition;
+        float contentHeightDifference = (m_ContentRectTransform.rect.height - m_RectTransform.rect.height);
+
+        float selectedPosition = (m_ContentRectTransform.rect.height - selectedDifference.y);
+        float currentScrollRectPosition = m_ScrollRect.normalizedPosition.y * contentHeightDifference;
+        float above = currentScrollRectPosition - (m_SelectedRectTransform.rect.height / 2) + m_RectTransform.rect.height;
+        float below = currentScrollRectPosition + (m_SelectedRectTransform.rect.height / 2);
+
+        // check if selected is out of bounds
+        if (selectedPosition > above)
+        {
+            float step = selectedPosition - above;
+            float newY = currentScrollRectPosition + step;
+            float newNormalizedY = newY / contentHeightDifference;
+            m_ScrollRect.normalizedPosition = Vector2.Lerp(m_ScrollRect.normalizedPosition, new Vector2(0, newNormalizedY), scrollSpeed * Time.deltaTime);
+        }
+        else if (selectedPosition < below)
+        {
+            float step = selectedPosition - below;
+            float newY = currentScrollRectPosition + step;
+            float newNormalizedY = newY / contentHeightDifference;
+            m_ScrollRect.normalizedPosition = Vector2.Lerp(m_ScrollRect.normalizedPosition, new Vector2(0, newNormalizedY), scrollSpeed * Time.deltaTime);
+        }
+    }
+}
